213b6ab4478e6017bc2060b89e805c7bae384cca
Bayan Ghaben <127290963+bayanghaben@users.noreply.github.com>
2025-10-28 23:07:17 +0300

    fix configs of tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
-  theme: {
-    extend: {},
-  },
+  content: ["./index.html", "./src/**/*.{js,jsx}"],
+  mode: "jit",
   plugins: [],
 };
